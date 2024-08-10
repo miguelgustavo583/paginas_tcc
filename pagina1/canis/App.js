20b24ac79bf35991b05019bfac1c6502f877062b
miguelgustavo583 <miguelgscampos@gmail.com>
2024-08-09 21:46:00 -0300

    add pagina1 atualizada

--- pagina1/canis/App.js
+++ pagina1/canis/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 
 const WelcomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require('./assets/images/logo.png')} style={styles.logo} />
-      <Text style={styles.welcomeText}>Bem-Vindo</Text>
-      <Text style={styles.nameText}>Pitico</Text>
-    </View>
+    <ImageBackground source={require('./assets/images/fundo.png')} style={styles.background}>
+      <View style={styles.container}>
+        <Image source={require('./assets/images/logo.png')} style={styles.logo} />
+        <Text style={styles.welcomeText}>Bem-Vindo</Text>
+        <Text style={styles.nameText}>Pitico</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#8B0000', 
   },
   logo: {
-    width: 50, 
-    height: 40, 
+    width: 50,
+    height: 40,
     marginBottom: 20,
   },
   welcomeText: {
@@ -36,8 +42,3 @@ const styles = StyleSheet.create({
 });
 
 export default WelcomeScreen;
-
-
-
-
-
